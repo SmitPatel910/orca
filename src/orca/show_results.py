3b7d2b0211567d09b6e46a0b27891751b6d88f51
SmitPatel910 <146316472+se-doubleblinder@users.noreply.github.com>
2024-03-21 18:41:56 -0500

    added other rq results

--- src/orca/show_results.py
+++ src/orca/show_results.py
@@ -1,5 +1,5 @@
 import json
-from utils import get_statements_from_blocks
+from utils import get_statements_from_blocks, get_scope
 from accuracy import get_statement_coverage, get_statement_prefix
 
 with open('./output/output_fixeval_cfg.json', 'r') as file:
@@ -97,5 +97,65 @@ print()
 # RQ3
 print("RQ3")
 print(f"Symbol Table: {100 * (ST/count):.2f}")
-print(f"Total Symbol table: {symbol_table_count:.2f}")
-print(f"Correct Error Block and ST is 100 : {len(special_list)}")
+
+# RQ4
+total = 0
+type_statements = {
+    'for': {"correct": 0, "incorrect": 0, "total": 0},
+    'while': {"correct": 0, "incorrect": 0, "total": 0},
+    'if': {"correct": 0, "incorrect": 0, "total": 0},
+    'simple': {"correct": 0, "incorrect": 0, "total": 0}
+}
+
+for key in response_cache.keys():
+    for sub in response_cache[key].keys():
+        if response_cache[key][sub]['accuracy'] == {}: continue
+        EB = response_cache[key][sub]['accuracy']['EB']
+        gt_execution = dataset[key][sub]['ground_truth_execution_order']
+        code = dataset[key][sub]['code']
+        for_loop, while_loop, if_statement, simple_statement = get_scope(code)
+        line_number = gt_execution[-1]
+        type_statement = ""
+        for scope in for_loop:
+            if scope[0] <= line_number <= scope[1]:
+                type_statement = "for"
+                break
+        for scope in while_loop:
+            if scope[0] <= line_number <= scope[1]:
+                type_statement = "while"
+                break
+        for scope in if_statement:
+            if scope[0] <= line_number <= scope[1]:
+                type_statement = "if"
+                break
+        for scope in simple_statement:
+            if scope[0] <= line_number <= scope[1]:
+                type_statement = "simple"
+                break
+        total += 1
+        try:
+            if EB == 1:
+                type_statements[type_statement]["correct"] += 1
+                type_statements[type_statement]["total"] += 1
+            else:
+                type_statements[type_statement]["incorrect"] += 1
+                type_statements[type_statement]["total"] += 1
+        except:
+            continue
+
+
+
+# Accuracy Analysis
+for_pre = f"{100 * (type_statements['for']['correct'] / type_statements['for']['total']):.2f}"
+while_pre = f"{100 * (type_statements['while']['correct'] / type_statements['while']['total']):.2f}"
+loop_pre = for_pre + while_pre
+if_pre = f"{100 * (type_statements['if']['correct'] / type_statements['if']['total']):.2f}"
+simple_pre = f"{100 * (type_statements['simple']['correct'] / type_statements['simple']['total']):.2f}"
+
+print()
+print("Type:        FOR       WHILE       IF       SIMPLE")
+print(f"Instances:   {type_statements['for']['total']}        {type_statements['while']['total']}         {type_statements['if']['total']}         {type_statements['simple']['total']} ")
+print('---------------------------------------------------------')
+print(f"Correct:      {type_statements['for']['correct']}        {type_statements['while']['correct']}         {type_statements['if']['correct']}          {type_statements['simple']['correct']}")
+print(f"Acc(%):    ({(for_pre)}%)  ({(while_pre)}%)  ({(if_pre)}%)    ({(simple_pre)}%)")
+print()
--- src/orca/utils.py
+++ src/orca/utils.py
@@ -1,6 +1,7 @@
 import re
 import os
 import json
+import ast
 
 tokens_path = os.path.join(os.path.dirname(__file__), 'output' ,'tokens.json')
 
@@ -194,4 +195,60 @@ def get_statements_from_blocks(block_range, prediction_blocks, ground_truth_bloc
 
     return pd_statement_execution, gt_statement_execution
 
+# Get the Scope of the Branches From the Code
+def extract_scopes_with_line_numbers(code):
+    tree = ast.parse(code)
+    categorized_scopes = {
+        'for': [],
+        'while': [],
+        'if': [],
+        'simple_statement': []
+    }
+
+    class ScopeExtractor(ast.NodeVisitor):
+        def extract_scope(self, node, scope_type):
+            start_line = node.lineno
+            end_line = node.end_lineno
+            scope_code = ast.get_source_segment(code, node)
+            categorized_scopes[scope_type].append((start_line, end_line, scope_code))
+
+        def visit_For(self, node):
+            self.extract_scope(node, 'for')
+
+        def visit_While(self, node):
+            self.extract_scope(node, 'while')
+
+        def visit_If(self, node):
+            self.extract_scope(node, 'if')
+
+        def generic_visit(self, node):
+            if isinstance(node, ast.Expr) or isinstance(node, ast.Assign) or isinstance(node, ast.AugAssign):
+                start_line = node.lineno
+                end_line = node.end_lineno
+                stmt_code = ast.get_source_segment(code, node)
+                categorized_scopes['simple_statement'].append((start_line, end_line, stmt_code))
+            super().generic_visit(node)
+
+    ScopeExtractor().visit(tree)
+    return categorized_scopes
+
+def get_scope(code):
+    for_loop = []
+    while_loop = []
+    if_statement = []
+    simple_statements = []
+
+    categorized_scopes = extract_scopes_with_line_numbers(code)
+    for scope_type, scopes in categorized_scopes.items():
+        for start_line, end_line, scope in scopes:
+            if scope_type == 'for':
+                for_loop.append([start_line, end_line])
+            elif scope_type == 'while':
+                while_loop.append([start_line, end_line])
+            elif scope_type == 'if':
+                if_statement.append([start_line, end_line])
+            else:
+                simple_statements.append([start_line, end_line])
+        
+    return for_loop, while_loop, if_statement, simple_statements
 
