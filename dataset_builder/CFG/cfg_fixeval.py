5f82abe54173e765530018f1a495db666a9d9aaa
sedoubleblinder <164423280+sedoubleblinder@users.noreply.github.com>
2024-03-23 06:01:55 -0500

    Update cfg_fixeval.py

--- dataset_builder/CFG/cfg_fixeval.py
+++ dataset_builder/CFG/cfg_fixeval.py
@@ -33,58 +33,44 @@ def make_cfg(code, Code):
         return None, None, None, None
  
 if __name__ == '__main__':
-    input_prefers = input("Do you want to use CFG for particular program? (y/n): ")
-    if input_prefers == 'y':
-        from example import testFun
-        graph = control_flow.get_control_flow_graph(testFun)
-        source = program_utils.getsource(testFun)
-        control_flow_graphviz.render(graph, include_src=source, path='cfg.png')
-        with open('example.py', 'r') as file:
-            code = file.read()
-        cfg_block_statements, cfg_block_range, cfg_next_block = read_cfg(graph, code)
-        f_cfg_block_statements, f_cfg_block_range, f_cfg_next_block = renumbering_blocks(cfg_block_statements, cfg_block_range, cfg_next_block)
-        input_cfg = make_cfg_into_code(f_cfg_block_statements, f_cfg_next_block, f_cfg_block_range)
-        if input_cfg == "": print("Error")
-    else:
-        with open('../../dataset/fixeval_crash_for_cfg.json','r') as file:
-            fixeval_dataset = json.load(file)
-        # cfg_dataset
-        count = 0
-        total = 0
-        true = 0
-        final_dataset = {}
-        for prob_id in tqdm(fixeval_dataset):
-            if fixeval_dataset[prob_id] == {}: continue
-            final_dataset[prob_id] = {}
-            submissions = {}
-            for sub_id in fixeval_dataset[prob_id]:
-                if fixeval_dataset[prob_id][sub_id] == {}: continue
-                total += 1
-                code = fixeval_dataset[prob_id][sub_id]['code']
-                execution_order = fixeval_dataset[prob_id][sub_id]['execution_order']
-                exe_trace = fixeval_dataset[prob_id][sub_id]['final_trace']
-                cfg_block_statements, cfg_block_range, cfg_next_block, input_cfg = make_cfg(code, code)
-                if cfg_block_statements == None or cfg_block_range == None or cfg_next_block == None or input_cfg == None: count += 1; continue
-                # Modify the ground truth into the blocks
-                ground_truth_blocks = ground_truth_code_blocks_fixeval(cfg_block_range, execution_order, exe_trace)
-                true += 1
-                temp = {
-                    'code': code,
-                    'cfg_block_range': cfg_block_range,
-                    'ground_truth_execution_order': execution_order,
-                    'ground_truth_blocks': ground_truth_blocks,
-                    'exception_info': fixeval_dataset[prob_id][sub_id]['exception_info'],
-                    'cfg_block_statements': cfg_block_statements,
-                    'cfg_next_block': cfg_next_block,
-                    'input_cfg': input_cfg
-                }
-                submissions[sub_id] = temp
-            final_dataset[prob_id] = submissions
+    with open('../../dataset/fixeval_crash_for_cfg.json','r') as file:
+        fixeval_dataset = json.load(file)
+    # cfg_dataset
+    count = 0
+    total = 0
+    true = 0
+    final_dataset = {}
+    for prob_id in tqdm(fixeval_dataset):
+        if fixeval_dataset[prob_id] == {}: continue
+        final_dataset[prob_id] = {}
+        submissions = {}
+        for sub_id in fixeval_dataset[prob_id]:
+            if fixeval_dataset[prob_id][sub_id] == {}: continue
+            total += 1
+            code = fixeval_dataset[prob_id][sub_id]['code']
+            execution_order = fixeval_dataset[prob_id][sub_id]['execution_order']
+            exe_trace = fixeval_dataset[prob_id][sub_id]['final_trace']
+            cfg_block_statements, cfg_block_range, cfg_next_block, input_cfg = make_cfg(code, code)
+            if cfg_block_statements == None or cfg_block_range == None or cfg_next_block == None or input_cfg == None: count += 1; continue
+            # Modify the ground truth into the blocks
+            ground_truth_blocks = ground_truth_code_blocks_fixeval(cfg_block_range, execution_order, exe_trace)
+            true += 1
+            temp = {
+                'code': code,
+                'cfg_block_range': cfg_block_range,
+                'ground_truth_execution_order': execution_order,
+                'ground_truth_blocks': ground_truth_blocks,
+                'exception_info': fixeval_dataset[prob_id][sub_id]['exception_info'],
+                'cfg_block_statements': cfg_block_statements,
+                'cfg_next_block': cfg_next_block,
+                'input_cfg': input_cfg
+            }
+            submissions[sub_id] = temp
+        final_dataset[prob_id] = submissions
 
-        print(f"Total Instances: {total}")
-        print(f"True Instances: {true}")
-        print(f"False Instances: {count}")
-        
-        with open('../../dataset/fixeval_crash_for_sample.json','w') as file:
-            json.dump(final_dataset, file)
-
+    print(f"Total Instances: {total}")
+    print(f"True Instances: {true}")
+    print(f"False Instances: {count}")
+    
+    with open('../../dataset/fixeval_crash_for_sample.json','w') as file:
+        json.dump(final_dataset, file)
