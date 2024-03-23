4cfe832bad5d9bd283c7bc3c22d34fb0e7cba9e3
sedoubleblinder <164423280+sedoubleblinder@users.noreply.github.com>
2024-03-23 05:44:54 -0500

    Update script.py

--- dataset_builder/script.py
+++ dataset_builder/script.py
@@ -1,6 +1,8 @@
 import json
 from utils import analyze_code, remove_comments_and_blank_lines, check_code_conditions, filter_code_based_on_input_lines
 from variable_locator import caller_function
+from tqdm import tqdm
+
 with open ('../dataset/structured_fixeval_dataset.json', 'r') as f:
     structured_data = json.load(f)
 
@@ -11,7 +13,7 @@ true_count = 0
 filtered_data = {}
 
 # Filter Dataset based on the loops, method calls, and other conditions
-for item in structured_data:
+for item in tqdm(structured_data):
     if item not in filtered_data:
         filtered_data[item] = {} 
     for submission in structured_data[item]:
@@ -32,42 +34,12 @@ for item in structured_data:
         except Exception as e:
             false_count += 1
             pass
-print(f"Total Instances in Structured Data: {total_count}")
 
 # Filtering dataset based on problem ids
 h_data = filtered_data
-with open('../dataset/previous_work_dataset.json', 'r') as f:
-    s_data = json.load(f)
-   
-collected_problems = {}
-for key in h_data.keys():
-    if key in s_data:
-        collected_problems[key] = {}
-
-total_count_brefore_filter = true_count
-problem_submission_count = 0
-false_count = 0
-for key in collected_problems.keys():
-    for submission in h_data[key]:
-        try:
-            submission_block = h_data[key][submission]
-            code = submission_block['code']
-            filtered_code = remove_comments_and_blank_lines(code)
-            functions_class = submission_block['functions_class']
-            functions_standalone = submission_block['functions_standalone']
-            verdict = submission_block['verdict']
-            collected_problems[key][submission] = {'code': filtered_code, 'functions_class': functions_class, 'functions_standalone': functions_standalone, 'verdict': verdict} 
-            problem_submission_count += 1
-        except Exception as e:
-            false_count += 1
-            pass
-
 # Get the Input variables line from the code
-variable_locations = caller_function(collected_problems)
+variable_locations = caller_function(h_data)
 # Filter the dataset based on the input variables location
 final_dataset, true_count = filter_code_based_on_input_lines(variable_locations)
-print(f"Total: {problem_submission_count}, True: {true_count}")
-
 with open('../dataset/filtered_fixeval_dataset.json', 'w') as f:
     json.dump(final_dataset, f, indent=4)
-
